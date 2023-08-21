5372d0c455831f8a6dc431213520501680d53591
hong-sile <gurwns9325@naver.com>
2023-08-21 15:49:10 +0900

    feat: initial project

---
+++ src/main/java/jap_proxy/SpringApplication.java
@@ -0,0 +1,10 @@
+package jap_proxy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplication {
+
+  public static void main(String[] args) {
+  }
+}
---
+++ src/test/java/jap_proxy/SpringApplicationTest.java
@@ -0,0 +1,14 @@
+package jap_proxy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTest {
+
+  @Test
+  @DisplayName("boot Test")
+  void Test() {
+  }
+}
