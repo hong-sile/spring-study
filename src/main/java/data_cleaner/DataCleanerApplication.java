e3962808375d7444e55f237d207c6ce7ca3b082a
hong-sile <gurwns9325@naver.com>
2023-11-01 14:47:06 +0900

    feat: 멀티모듈 세팅

---
+++ src/main/java/data_cleaner/DataCleanerApplication.java
@@ -0,0 +1,12 @@
+package data_cleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCleanerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DataCleanerApplication.class);
+  }
+}
--- src/main/java/jpa_proxy/member/MemberRepository.java
+++ src/main/java/jpa_proxy/member/MemberRepository.java
@@ -3,7 +3,9 @@ package jpa_proxy.member;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
   @Query("select m From Member m")
