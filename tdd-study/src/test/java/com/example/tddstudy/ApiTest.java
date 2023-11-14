499820374939521b25e1862b8859dc21cd86e408
hong-sile <gurwns9325@naver.com>
2023-11-14 12:01:02 +0900

    refactor: DataCleanerExtension 추가

--- tdd-study/src/test/java/com/example/tddstudy/ApiTest.java
+++ tdd-study/src/test/java/com/example/tddstudy/ApiTest.java
@@ -4,9 +4,11 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+@ExtendWith(DataCleanerExtension.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class ApiTest {
 
