d4d9722e38e7490a188c44a09b113b57f7b0b76e
hong-sile <gurwns9325@naver.com>
2023-11-14 12:00:36 +0900

    test: 데이터베이스 데이터 초기화를 위한 DataCleaner 클래스 관련 파일 추가

---
+++ tdd-study/src/test/java/com/example/tddstudy/DatabaseCleaner.java
@@ -0,0 +1,44 @@
+package com.example.tddstudy;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DatabaseCleaner {
+
+  private final List<String> tableNames = new ArrayList<>();
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @PostConstruct
+  @SuppressWarnings("unchecked")
+  private void findDatabaseTableNames() {
+    final List<Object[]> tableInfos = entityManager.createNativeQuery("SHOW TABLES")
+        .getResultList();
+    for (final Object[] tableInfo : tableInfos) {
+      String tableName = (String) tableInfo[0];
+      tableNames.add(tableName);
+    }
+  }
+
+  private void truncate() {
+    entityManager.createNativeQuery(String.format("SET FOREIGN_KEY_CHECKS %d", 0)).executeUpdate();
+    for (String tableName : tableNames) {
+      entityManager.createNativeQuery(String.format("TRUNCATE TABLE %s", tableName))
+          .executeUpdate();
+    }
+    entityManager.createNativeQuery(String.format("SET FOREIGN_KEY_CHECKS %d", 1)).executeUpdate();
+  }
+
+  @Transactional
+  public void clear() {
+    entityManager.clear();
+    truncate();
+  }
+}
