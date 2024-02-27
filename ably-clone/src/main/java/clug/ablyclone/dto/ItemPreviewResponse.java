7cf33d70da6927394fbbb0d38f7d806e3ec5a9d9
hong-sile <hyukjoon.dev@gmail.com>
2024-02-28 01:20:55 +0900

    feat: Item 형태를 변경하는 기능 추가

--- ably-clone/src/main/java/clug/ablyclone/dto/ItemPreviewResponse.java
+++ ably-clone/src/main/java/clug/ablyclone/dto/ItemPreviewResponse.java
@@ -1,5 +1,6 @@
 package clug.ablyclone.dto;
 
+import clug.ablyclone.domain.Item;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,9 +8,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ItemPreviewResponse {
 
-  private final String discountRate;
+  private static final String DISCOUNT_PERCENTAGE_FORMAT = "%d%%";
+
+  private final String discountPercentage;
   private final String sellerName;
   private final String itemName;
   private final String imageUrl;
-  private final Long price;
+  private final Long discountedPrice;
+
+  public static ItemPreviewResponse from(final Item item) {
+    return new ItemPreviewResponse(
+        String.format(DISCOUNT_PERCENTAGE_FORMAT, item.getDiscountPercentage()),
+        item.getSeller().getName(),
+        item.getItemName(),
+        item.getImageUrl(),
+        item.getDiscountedPrice()
+    );
+  }
 }
